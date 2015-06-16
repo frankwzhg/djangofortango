6dbfa0265356660773eb6b4aeaeba86ae6cfd9d5
frankwzhg <frankwzhg@hotmail.com>
2015-06-16 08:41:07 +0800

    jquery and ajax

--- static/js/rango-ajax.js
+++ static/js/rango-ajax.js
@@ -6,4 +6,23 @@ $("#likes").click(function(){
         $("#like_count").html(data);
         $("#likes").hide();
     });
+
+});
+
+$('#suggestion').keyup(function(){
+        var query;
+        query = $(this).val();
+        $.get('/rango/suggest_category/', {suggestion: query}, function(data){
+         $('#cats').html(data);
+        });
+});
+
+$('.rango-add').click(function(){
+    var catid = $(this).attr("data-catid");
+    var title = $(this).atrr("data-title");
+    var url = $(this).attr("data-url");
+    $.get('/rango/auto_add_page/', {category_id: catid, url: url, title: title}, function(data){
+        $('#pages').html(data);
+        $(this).hide();
+    });
 });
